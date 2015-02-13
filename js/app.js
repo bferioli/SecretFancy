dd5e61926c4d5582d1fd1f2345560a312b9b6bc5
Brandon Ferioli <bferioli@gmail.com>
2015-02-13 13:18:54 -0500

    Login button refinements

--- js/app.js
+++ js/app.js
@@ -4,6 +4,14 @@ app.frontpage = {
 	views: {},
 	initAuthButton: function() {
 		this.dom.sendButton.removeAttr('disabled');
+
+		if (app.auth && app.auth.authenticated) {
+			this.dom.sendButton.text('Send a fancy');
+		} else {
+			this.dom.sendButton.text('Login with Facebook');
+			this.dom.sendButton.addClass('fb-auth');
+		}
+
 		this.dom.sendButton.on('click', $.proxy(this.newFancy, this));
 	},
 	createForm: function() {
@@ -15,7 +23,7 @@ app.frontpage = {
 		if (app.auth && app.auth.user)
 			this.createForm();
 		else if (app.auth)
-			app.auth.doLogin($.proxy(this.createForm, this));
+			app.auth.doLogin($.proxy(this.newFancy, this));
 	},
 	init: function() {
 		this.dom = {
