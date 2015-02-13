c21fdb30990700069cad68abee2d5efbb0435689
Brandon Ferioli <bferioli@gmail.com>
2015-02-13 13:34:21 -0500

    Add fancy message files

---
+++ js/viewFancy.js
@@ -0,0 +1,20 @@
+var app = app || {};
+
+app.viewFancy = {
+	model: null,
+	views: {},
+	init: function() {
+		// var href = window.location.href,
+		// 	id = href.substr(href.lastIndexOf('/') + 1);
+
+		var id = window.location.hash.substr(1);
+		
+		this.model = new Fancy({_id: id});
+		this.model.fetch();
+		this.views.model = new FancyMessage({model:this.model});
+	}
+};
+
+$(document).ready(function(){
+	app.viewFancy.init();
+});
---
+++ js/views/fancyMessage.js
@@ -0,0 +1,11 @@
+var FancyMessage = Backbone.View.extend({
+	el: '#fancy-message',
+	initialize: function(){
+		this.render();
+	},
+	render: function(){
+		var source = $('#FancyMessageTemplate').html();
+		var template = Handlebars.compile(source);
+		this.$el.html(template(this.model.toJSON()));
+	}
+});
