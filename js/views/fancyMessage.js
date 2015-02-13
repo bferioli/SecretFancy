7389669a9fcde0ed578e57bb606871c8d7d146fb
Brandon Ferioli <bferioli@gmail.com>
2015-02-13 14:02:33 -0500

    remove log

--- js/views/fancyMessage.js
+++ js/views/fancyMessage.js
@@ -7,6 +7,5 @@ var FancyMessage = Backbone.View.extend({
 		var source = $('#FancyMessageTemplate').html();
 		var template = Handlebars.compile(source);
 		this.$el.html(template(this.model.toJSON()));
-		console.log(this.model.toJSON());
 	}
 });
